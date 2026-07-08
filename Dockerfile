f03218ff04f9ea1bb2178493c47e3efefd809547
chenshuo <c1094829085@qq.com>
2026-07-08 21:40:38 +0200

    Fix local Docker Jekyll environment

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM ubuntu:latest
-ENV DEBIAN_FRONTEND noninteractive
+FROM ruby:3.2-bookworm
+ENV DEBIAN_FRONTEND=noninteractive
 
-Label MAINTAINER Amir Pourmand
+LABEL maintainer="Amir Pourmand"
 
 RUN apt-get update -y && apt-get install -y --no-install-recommends \
     locales \
     imagemagick \
-    ruby-full \
     build-essential \
     zlib1g-dev \
     jupyter-nbconvert \
@@ -23,17 +22,16 @@ ENV LANG=en_US.UTF-8 \
     LC_ALL=en_US.UTF-8 \
     JEKYLL_ENV=production
 
-# install jekyll and dependencies
-RUN gem install jekyll bundler
-
 RUN mkdir /srv/jekyll
-
-ADD Gemfile /srv/jekyll
 
 WORKDIR /srv/jekyll
 
-RUN bundle install --no-cache
-# && rm -rf /var/lib/gems/3.1.0/cache
+COPY Gemfile /srv/jekyll/
+
+RUN bundle config set jobs 4 && \
+    bundle config set retry 3 && \
+    bundle install
+
 EXPOSE 8080
 
 COPY bin/entry_point.sh /tmp/entry_point.sh
--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
+set -e
 
-CONFIG_FILE=_config.yml 
+echo "Entry point script running"
 
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+CONFIG_FILE=_config.yml
+JEKYLL_CMD="bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"
+
+/bin/bash -c "exec $JEKYLL_CMD"&
 
 while true; do
 
   inotifywait -q -e modify,move,create,delete $CONFIG_FILE
 
   if [ $? -eq 0 ]; then
- 
+
     echo "Change detected to $CONFIG_FILE, restarting Jekyll"
 
     jekyll_pid=$(pgrep -f jekyll)
-    kill -KILL $jekyll_pid
+    if [ -n "$jekyll_pid" ]; then
+      kill -KILL $jekyll_pid
+    fi
 
-    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+    /bin/bash -c "exec $JEKYLL_CMD"&
 
   fi
 
